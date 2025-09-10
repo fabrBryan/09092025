3ac01102a6ff6419de50211f70858e743a94449e
Fabryan <ahmad_pradana_34rpl@student.smktelkom-mlg.sch.id>
2025-09-10 09:14:03 +0700

    Add files via upload

--- Instruments.java
+++ Instruments.java
@@ -1,17 +1,37 @@
-
-
 public class Instruments {
+    //property/atribut/variabel
     private String name;
     private double price;
 
-    public Instruments (String name, double price){
-        this.name = name;
-        this.price = price;
-
+    //Default Constructor
+    public Instruments() {
+        name = "Piano";
+        price = 320;
     }
 
+    //Getter
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+
+    //Setter
+    public void setName(String n) {
+        name = n;
+    }
+    public void setPrice(double pr){
+        price = pr;
+    }
+
+    //Constructor Parameter
+    public Instruments(String n, double pr){
+        name = n;
+        price = pr;
+    }
     public void display (){
-       System.out.println("Name: " + name);
-       System.out.println("Price: " + price);
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
     }
 }
--- InstrumentsTest.java
+++ InstrumentsTest.java
@@ -2,10 +2,14 @@
 
 public class InstrumentsTest {
     public static void main(String[] args) {
-        Instruments piano = new Instruments("Piano", 350.00);
+        Instruments piano = new Instruments();
         piano.display();
-        Instruments guitar = new Instruments("Guitar",340.00);
+        Instruments guitar = new Instruments("Guitar",340);
         guitar.display();
-        System.out.println();
+
+        Instruments drum = new Instruments ();
+        drum.setName("Drum");
+        drum.setPrice(300);
+        drum.display();
     }
 }
